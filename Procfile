d1ea942c1a1fefc49a69cb554c8dde861e2838dc
Kenneth Galvez <clinic.directory.2021@gmail.com>
2021-12-01 07:06:22 +0800

    asdasdasda

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn churn_prediction_system.wsgi --log-file -
--- churn_prediction_system/churn_prediction_system/settings.py
+++ churn_prediction_system/churn_prediction_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t$c6-uilkkm*%6#c=jhz#__e#w=@&&eswj+seumr-u@t%&sbw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'churn-prediction-system.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'C:\Users\CANLAS\Documents\capstone_system\static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
